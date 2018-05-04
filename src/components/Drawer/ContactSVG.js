75324184575d52867eeaaafe4bfdd20611b24a28
Beauwie <kvn.philippe@gmail.com>
2018-05-04 13:22:35 +0200

    Change bottom part of navbar, add icons and github link

---
+++ src/components/Drawer/ContactSVG.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <svg height="15px" width="20px" version="1.1" viewBox="0 0 20 15">
+      <defs />
+      <g id="Page-1" fill="none" stroke="none" strokeWidth="1">
+        <g
+          id="Dribbble-Dark-Preview"
+          fill="#939393"
+          transform="translate(-340.000000, -922.000000)"
+        >
+          <g id="icons" transform="translate(56.000000, 160.000000)">
+            <path
+              id="email-[#1572]"
+              d="M294,774.474 L284,765.649 L284,777 L304,777 L304,765.649 L294,774.474 Z M294.001,771.812 L284,762.981 L284,762 L304,762 L304,762.981 L294.001,771.812 Z"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -3,26 +3,29 @@ import { connect } from 'react-redux';
 import styled from 'react-emotion';
 import Link from 'gatsby-link';
 
-const PaperContainer = styled.div`
+import ContactSVG from './ContactSVG';
+
+const SideBar = styled.aside`
   height: 100vh;
   width: 15vw;
 `;
 
-const Paper = styled.aside`
-  z-index: ${p => p.theme.zIndex.drawer};
+const NavBar = styled.div`
   position: fixed;
-  border-right: 5px solid red;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  border-right: 5px solid red;
   height: 100vh;
   width: 15vw;
 `;
 
-const Header = styled.header`
+const NavBarSection = styled.nav`
   display: flex;
-  align-items: center;
-  height: ${p => p.theme.size(4)};
+  flex-direction: column;
+  padding: 3rem 0;
 `;
+
 const BaseItem = styled(Link)`
   font-family: Helvetica;
   font-weight: 500;
@@ -32,24 +35,46 @@ const BaseItem = styled(Link)`
   text-align: right;
   text-transform: uppercase;
   text-decoration: none;
+
+  svg {
+    width: 2rem;
+    height: 2rem;
+  }
 `;
 const Item = styled(({ className, ...props }) => (
   <BaseItem {...props} activeClassName={className} />
 ))`
   color: red !important;
+  svg g g {
+    fill: red !important;
+  }
+`;
+
+const GithubLink = styled.a`
+  padding-right: 1rem;
+  text-align: right;
 `;
 
 export default () => (
-  <PaperContainer>
-    <Paper>
-      <Header />
-      <Item to="/" exact>
-        Home
-      </Item>
-      <Item to="/about/">About</Item>
-      <Item to="/projects/">Projects</Item>
-      <Item to="/skills/">Skills</Item>
-      <Item to="/contact/">contact</Item>
-    </Paper>
-  </PaperContainer>
+  <SideBar>
+    <NavBar>
+      <NavBarSection>
+        <Item to="/" exact>
+          Home
+        </Item>
+        <Item to="/about/">About</Item>
+        <Item to="/projects/">Projects</Item>
+        <Item to="/skills/">Skills</Item>
+      </NavBarSection>
+
+      <NavBarSection>
+        <Item to="/contact/">
+          <ContactSVG />
+        </Item>
+        <GithubLink href="https://github.com/Bauwi" target="_blank">
+          <img src="/images/github.png" alt="github logo" />
+        </GithubLink>
+      </NavBarSection>
+    </NavBar>
+  </SideBar>
 );
