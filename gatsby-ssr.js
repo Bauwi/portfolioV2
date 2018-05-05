35d57ceb950571e0095e77f829109c8226e93403
Beauwie <kvn.philippe@gmail.com>
2018-05-05 13:53:30 +0200

    Uninstall redux and unused dependancies

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { renderToString } from 'react-dom/server';
 import { extractCritical } from 'emotion-server';
-import createStore from './src/state/createStore';
 
 exports.replaceRenderer = ({
   bodyComponent,
   replaceBodyHTMLString,
   setHeadComponents,
 }) => {
-  const store = createStore();
-
   // Connect Redux store
   const ConnectedBody = () => (
     <Provider store={store}>{bodyComponent}</Provider>
